f841a70a728d804f9f165589678f1473d3675702
Administrator <Administrator@QLAMPC>
2017-09-14 21:00:23 +0700

    cap nhat

---
+++ src/main/java/com/donglam/webhoconline/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.donglam.webhoconline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index(Model model) {
+		//model.addAttribute("khoa", employeeService.findAll());
+		
+		return "index";
+	}
+	//modal login
+	@GetMapping("/loginregister")
+	public String login(Model model) {
+		return "loginregister";
+	}
+	// some other pages
+	@GetMapping("/contact")
+	public String contact(Model model) {
+		return "contact";
+	}
+	@GetMapping("/courses")
+	public String courses(Model model) {
+		return "courses";
+	}
+	@GetMapping("/myaccount")
+	public String myaccount(Model model) {
+		return "myaccount";
+	}
+	@GetMapping("/detailcourse")
+	public String detailcourse(Model model) {
+		return "detailcourse";
+	}
+	@GetMapping("/detailteacher")
+	public String detailteacher(Model model) {
+		return "detailteacher";
+	}
+	@GetMapping("/forum")
+	public String forum(Model model) {
+		return "forum";
+	}
+}
---
+++ src/main/java/com/donglam/webhoconline/model/LoaiKhoaHoc.java
@@ -0,0 +1,48 @@
+package com.donglam.webhoconline.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="loaikhoahoc")
+public class LoaiKhoaHoc {
+	@Id
+	@Column(name = "maloaikh", nullable = false)
+	private String maloaikh;
+	
+	@Column(name = "tenloaikh", nullable = false)
+	private String tenloaikh;
+	
+	@Column(name = "makhoa", nullable = false)
+	private String makhoa;
+
+	public LoaiKhoaHoc() {}
+	public LoaiKhoaHoc(String maloaikh, String tenloaikh, String makhoa) {
+		super();
+		this.maloaikh = maloaikh;
+		this.tenloaikh = tenloaikh;
+		this.makhoa = makhoa;
+	}
+	public String getMaloaikh() {
+		return maloaikh;
+	}
+	public void setMaloaikh(String maloaikh) {
+		this.maloaikh = maloaikh;
+	}
+	public String getTenloaikh() {
+		return tenloaikh;
+	}
+	public void setTenloaikh(String tenloaikh) {
+		this.tenloaikh = tenloaikh;
+	}
+	public String getMakhoa() {
+		return makhoa;
+	}
+	public void setMakhoa(String makhoa) {
+		this.makhoa = makhoa;
+	}
+	
+	
+}
